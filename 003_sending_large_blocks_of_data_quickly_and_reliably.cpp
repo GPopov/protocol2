993620c8566132cbee11ffe8f591b9d465ae271a
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-01-02 21:22:50 +1100

    wip on sending large blocks

--- 003_sending_large_blocks_of_data_quickly_and_reliably.cpp
+++ 003_sending_large_blocks_of_data_quickly_and_reliably.cpp
@@ -21,15 +21,14 @@ const int NumIterations = 32;
 
 const uint32_t ProtocolId = 0x11223344;
 
-enum TestPacketTypes
+enum PacketTypes
 {
-    TEST_PACKET_CHUNK_SLICE,                    // this packet contains slice x out of y that makes up chunk n
-    TEST_PACKET_CHUNK_ACK,                      // this packet acks a slice as being received by the other side
-
-    TEST_PACKET_NUM_TYPES
+    SLICE_PACKET,                    // this packet contains slice x out of y that makes up chunk n
+    ACK_PACKET,                      // this packet acks slices of chunk n that have been received
+    NUM_PACKET_TYPES
 };
 
-struct TestPacketChunkSlice : public protocol2::Packet
+struct SlicePacket : public protocol2::Packet
 {
     uint16_t chunkId;
     int sliceId;
@@ -37,7 +36,7 @@ struct TestPacketChunkSlice : public protocol2::Packet
     int sliceBytes;
     uint8_t data[ChunkSliceSize];
 
-    TestPacketChunkSlice() : Packet( TEST_PACKET_CHUNK_SLICE )
+    SlicePacket() : Packet( SLICE_PACKET )
     {
         chunkId = 0;
         sliceId = 0;
@@ -64,16 +63,16 @@ struct TestPacketChunkSlice : public protocol2::Packet
     }
 };
 
-// IMPORTANT: because acks can be lost, must always when a slice is received for a chunk
+// IMPORTANT: because acks can be lost, must always set a dirty flag when a slice is received for the current chunk id
 
-struct TestPacketChunkAck : public protocol2::Packet
+struct AckPacket : public protocol2::Packet
 {
     uint16_t chunkId;
     int sliceId;
     int numSlices;
     bool acked[MaxSlicesPerChunk];
 
-    TestPacketChunkAck() : Packet( TEST_PACKET_CHUNK_ACK )
+    AckPacket() : Packet( ACK_PACKET )
     {
         chunkId = 0;
         sliceId = 0;
@@ -92,18 +91,83 @@ struct TestPacketChunkAck : public protocol2::Packet
     }
 };
 
-struct TestPacketFactory : public protocol2::PacketFactory
+struct PacketFactory : public protocol2::PacketFactory
 {
-    TestPacketFactory() : PacketFactory( TEST_PACKET_NUM_TYPES ) {}
+    PacketFactory() : protocol2::PacketFactory( NUM_PACKET_TYPES ) {}
 
     protocol2::Packet* CreateInternal( int type )
     {
         switch ( type )
         {
-            case TEST_PACKET_CHUNK_SLICE:   return new TestPacketChunkSlice();
-            case TEST_PACKET_CHUNK_ACK:     return new TestPacketChunkAck();
+            case SLICE_PACKET:   return new SlicePacket();
+            case ACK_PACKET:     return new AckPacket();
         }
         return NULL;
+    }
+};
+
+static PacketFactory packetFactory;
+
+class ChunkSender
+{
+    bool sending;                                               // true if we are currently sending a chunk. can only send one chunk at a time
+    uint16_t chunkId;                                           // the chunk id. starts at 0 and increases as each chunk is successfully sent and acked.
+    int chunkSize;                                              // the size of the chunk that is being sent in bytes
+    int sliceId;                                                // the slice of the chunk that is to be sent next. iteration is from left -> right wrapping past the last slice back to 0.
+    int numSlices;                                              // the number of slices in the current chunk being sent
+    bool acked[MaxSlicesPerChunk];                              // acked flag for each slice of the chunk. chunk send completes when all slices are acked. acked slices are skipped when iterating for next slice to send.
+    double timeLastSent[MaxSlicesPerChunk];                     // time the slice of the chunk was last sent. avoids redundant behavior
+    uint8_t chunkData[MaxSlicesPerChunk*ChunkSliceSize];        // chunk data in 
+
+    ChunkSender()
+    {
+        memset( this, 0, sizeof( ChunkSender ) );
+    }
+
+    void SendChunk()
+    {
+        assert( SendCompleted() );
+
+        // ...
+    }
+
+    bool SendCompleted()
+    {
+        return !sending;
+    }
+
+    // todo: make sure the algorithm iterates from left -> right always in passes
+    // vs. continuously iterating across the same slices left -> right always
+
+    SlicePacket* SendSlicePacket( double t )
+    {
+        // todo: might not be time to send another slice yet, in which case return NULL?
+
+        // ...
+
+        return NULL;
+    }
+
+    void ProcessAckPacket( AckPacket *packet )
+    {
+        // ...
+    }
+};
+
+class ChunkReceiver
+{
+    uint16_t chunkId;
+
+    AckPacket* SendAckPacket()
+    {
+        // ...
+
+        return NULL;
+    }
+
+    void ProcessSlicePacket( SlicePacket *packet )
+    {
+        // ...
     }
 };
 
@@ -114,10 +178,6 @@ struct TestPacketFactory : public protocol2::PacketFactory
 int main()
 {
     srand( time( NULL ) );
-
-    TestPacketFactory packetFactory;
-
-    uint16_t sequence = 0;
 
     for ( int i = 0; i < NumIterations; ++i )
     {
