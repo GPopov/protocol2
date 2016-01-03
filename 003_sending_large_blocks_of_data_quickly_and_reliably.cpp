cc53206640e7e0d109fd0be3e4ff8cab5d4a40f8
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-01-03 13:29:56 +1100

    wip on 003

--- 003_sending_large_blocks_of_data_quickly_and_reliably.cpp
+++ 003_sending_large_blocks_of_data_quickly_and_reliably.cpp
@@ -19,6 +19,7 @@ const int MaxSlicesPerChunk = 256;
 const int MaxChunkSize = ChunkSliceSize * MaxSlicesPerChunk;
 
 const float SliceMinimumResendTime = 0.1f;
+const float MinimumTimeBetweenAcks = 0.1f;
 
 const uint32_t ProtocolId = 0x11223344;
 
@@ -63,8 +64,6 @@ struct SlicePacket : public protocol2::Packet
         return true;
     }
 };
-
-// IMPORTANT: because acks can be lost, must always set a dirty flag when a slice is received for the current chunk id
 
 struct AckPacket : public protocol2::Packet
 {
@@ -116,6 +115,7 @@ class ChunkSender
     int chunkSize;                                              // the size of the chunk that is being sent in bytes
     int numSlices;                                              // the number of slices in the current chunk being sent
     int currentSliceId;                                         // the current slice id to be considered next time we send a slice packet. iteration starts here.
+    int numAckedSlices;                                         // number of slices acked by the receiver. when num slices acked = num slices, the send is completed.
     bool acked[MaxSlicesPerChunk];                              // acked flag for each slice of the chunk. chunk send completes when all slices are acked. acked slices are skipped when iterating for next slice to send.
     double timeLastSent[MaxSlicesPerChunk];                     // time the slice of the chunk was last sent. avoids redundant behavior
     uint8_t chunkData[MaxChunkSize];                            // chunk data being sent.
@@ -136,12 +136,14 @@ public:
         sending = true;
         chunkSize = size;
         currentSliceId = 0;
+        numAckedSlices = 0;
         numSlices = ( size + ChunkSliceSize - 1 ) / ChunkSliceSize;
         assert( numSlices > 0 );
         assert( numSlices < MaxSlicesPerChunk );
         memset( acked, 0, sizeof( acked ) );
         memset( timeLastSent, 0, sizeof( timeLastSent ) );
         memcpy( chunkData, data, size );
+        printf( "sending chunk %d of size %d bytes in %d slices\n", chunkId, chunkSize, numSlices );
     }
 
     bool SendCompleted()
@@ -185,10 +187,32 @@ public:
     {
         assert( packet );
 
+        if ( !sending )
+            return false;
+
         if ( packet->chunkId != chunkId )
             return false;
 
+        if ( packet->numSlices != numSlices )
+            return false;
 
+        for ( int i = 0; i < numSlices; ++i )
+        {
+            if ( acked[i] == false && packet->acked[i] )
+            {
+                printf( "acked slice %d of chunk %d\n", i, chunkId );
+                acked[i] = true;
+                numAckedSlices++;
+                assert( numAckedSlices >= 0 );
+                assert( numAckedSlices <= numSlices );
+                if ( numAckedSlices == numSlices )
+                {
+                    printf( "all slices of chunk %d acked, send completed\n", chunkId );
+                    sending = false;
+                    chunkId++;
+                }
+            }
+        }
 
         return true;
     }
@@ -196,24 +220,128 @@ public:
 
 class ChunkReceiver
 {
-    uint16_t chunkId;
+    bool receiving;                                             // true if we are currently receiving a chunk.
+    bool readyToRead;                                           // true if a chunk has been received and is ready for the caller to read.
+    bool forceAckPreviousChunk;                                 // if this flag is set then we need to send a complete ack for the previous chunk id (sender has not yet received an ack with all slices received)
+    uint16_t chunkId;                                           // id of the chunk that is currently being received, or
+    int chunkSize;                                              // the size of the chunk that has been received. only known once the last slice has been received!
+    int numSlices;                                              // the number of slices in the current chunk being sent
+    int numReceivedSlices;                                      // number of slices received for the current chunk. when num slices receive = num slices, the receive is complete.
+    double timeLastAckSent;                                     // time last ack was sent. used to rate limit acks to some maximum number of acks per-second. 
+    bool received[MaxSlicesPerChunk];                           // received flag for each slice of the chunk. chunk receive completes when all slices are received.
+    uint8_t chunkData[MaxChunkSize];                            // chunk data being received.
 
 public:
 
-    AckPacket* SendAckPacket()
+    ChunkReceiver()
     {
-        // ...
+        memset( this, 0, sizeof( ChunkReceiver ) );
+    }
+
+    bool ProcessSlicePacket( SlicePacket *packet )
+    {
+        assert( packet );
+
+        // caller has read the chunk before we can receive the next one
+        if ( readyToRead )
+            return false;
+
+        if ( !receiving && packet->chunkId == chunkId - 1 )
+        {
+            // this happens because the sender has not yet seen an ack for all slices (packet loss) even though the sender has received them all.
+            // if we don't force an ack, the sender will be stuck with unacked slices from the previous chunk and won't ever complete the send.
+            forceAckPreviousChunk = true;
+        }
+
+        if ( !receiving && packet->chunkId == chunkId )
+        {
+            printf( "started receiving chunk %d\n", chunkId );
+
+            assert( !readyToRead );
+            
+            receiving = true;
+            forceAckPreviousChunk = false;
+            numReceivedSlices = 0;
+            chunkSize = 0;
+            
+            numSlices = packet->numSlices;
+            assert( numSlices > 0 );
+            assert( numSlices <= MaxSlicesPerChunk );
+
+            memset( received, 0, sizeof( received ) );
+        }
+
+        if ( packet->chunkId != chunkId )
+            return false;
+
+        if ( packet->numSlices != numSlices )
+            return false;
+
+        assert( packet->sliceId >= 0 );
+        assert( packet->sliceId <= numSlices );
+
+        if ( !received[packet->sliceId] )
+        {
+            printf( "received slice %d of chunk %d\n", packet->sliceId, chunkId );
+
+            received[packet->sliceId] = true;
+
+            numReceivedSlices++;
+
+            assert( numReceivedSlices > 0 );
+            assert( numReceivedSlices <= numSlices );
+
+            if ( packet->sliceId == numSlices - 1 )
+                chunkSize = ( numSlices - 1 ) * packet->sliceBytes;
+
+            if ( numReceivedSlices == numSlices )
+            {
+                receiving = false;
+                readyToRead = true;
+                chunkId++;
+            }
+        }
+
+        return true;
+    }
+
+    AckPacket* SendAckPacket( double t )
+    {
+        if ( timeLastAckSent + MinimumTimeBetweenAcks < t )
+            return NULL;
+
+        if ( forceAckPreviousChunk )
+        {
+            // todo: send a complete ack for the previous chunk
+
+            // ...
+
+            timeLastAckSent = t;
+        }
+
+        if ( receiving )
+        {
+            // todo: send an ack for the chunk currently being received
+
+            timeLastAckSent = t;
+        }
 
         return NULL;
     }
 
-    void ProcessSlicePacket( SlicePacket *packet )
+    const uint8_t* ReadChunk( uint16_t & chunkId, int & chunkSize )
     {
-        // ...
+        if ( !readyToRead )
+            return NULL;
+
+        readyToRead = false;
+
+        chunkId = this->chunkId - 1;        // because we already advanced it once all slices were received
+        chunkSize = this->chunkSize;
+
+        return chunkData;
     }
 };
-
-// todo: need to split into sender and receiver structs
 
 // todo: going to need a network simulator for this example (out of order, packet loss, latency...)
 
