05d1c69a3529986ed3ebf6b4656a3686e364f025
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-01-06 16:03:28 +1100

    wip on packet aggregation example

---
+++ 004_packet_aggregation.cpp
@@ -0,0 +1,287 @@
+/*
+    Example source code for "Packet Aggregation"
+    This software is in the public domain. Where that dedication is not recognized, 
+    you are granted a perpetual, irrevocable license to copy, distribute, and modify this file as you see fit.
+*/
+
+#define PROTOCOL2_IMPLEMENTATION
+
+#include "protocol2.h"
+#include <stdio.h>
+#include <time.h>
+
+const int NumIterations = 16;
+
+const uint32_t MaxPacketSize = 1024;
+
+const uint32_t ProtocolId = 0x12345678;
+
+enum TestPacketTypes
+{
+    TEST_PACKET_A,
+    TEST_PACKET_B,
+    TEST_PACKET_C,
+    TEST_PACKET_NUM_TYPES
+};
+
+struct Vector
+{
+    float x,y,z;
+};
+
+inline int random_int( int min, int max )
+{
+    assert( max > min );
+    int result = min + rand() % ( max - min + 1 );
+    assert( result >= min );
+    assert( result <= max );
+    return result;
+}
+
+inline float random_float( float min, float max )
+{
+    const int res = 10000000;
+    double scale = ( rand() % res ) / double( res - 1 );
+    return (float) ( min + (double) ( max - min ) * scale );
+}
+
+struct TestPacketA : public protocol2::Packet
+{
+    int a,b,c;
+
+    TestPacketA() : Packet( TEST_PACKET_A )
+    {
+        a = random_int( -10, +10 );
+        b = random_int( -20, +20 );
+        c = random_int( -30, +30 );
+    }
+
+    PROTOCOL2_SERIALIZE_FUNCTION( stream )
+    {
+        serialize_int( stream, a, -10, 10 );
+        serialize_int( stream, b, -20, 20 );
+        serialize_int( stream, c, -30, 30 );
+        return true;
+    }
+
+    bool operator == ( const TestPacketA & other ) const
+    {
+        return a == other.a && b == other.b && c == other.c;
+    }
+
+    bool operator != ( const TestPacketA & other ) const
+    {
+        return ! ( *this == other );
+    }
+};
+
+static const int MaxItems = 16;
+
+struct TestPacketB : public protocol2::Packet
+{
+    int numItems;
+    int items[MaxItems];
+
+    TestPacketB() : Packet( TEST_PACKET_B )
+    {
+        numItems = random_int( 0, MaxItems );
+        for ( int i = 0; i < numItems; ++i )
+            items[i] = random_int( -100, +100 );
+    }
+
+    PROTOCOL2_SERIALIZE_FUNCTION( stream )
+    {
+        serialize_int( stream, numItems, 0, MaxItems );
+        for ( int i = 0; i < numItems; ++i )
+            serialize_int( stream, items[i], -100, +100 );
+        return true;
+    }
+
+    bool operator == ( const TestPacketB & other ) const
+    {
+        if ( numItems != other.numItems )
+            return false;
+        for ( int i = 0; i < numItems; ++i )
+        {
+            if ( items[i] != other.items[i] )
+                return false;
+        }
+        return true;
+    }
+
+    bool operator != ( const TestPacketB & other ) const
+    {
+        return ! ( *this == other );
+    }
+};
+
+struct TestPacketC : public protocol2::Packet
+{
+    Vector position;
+    Vector velocity;
+
+    TestPacketC() : Packet( TEST_PACKET_C )
+    {
+        position.x = random_float( -1000, +1000 );
+        position.y = random_float( -1000, +1000 );
+        position.z = random_float( -1000, +1000 );
+
+        if ( rand() % 2 )
+        {
+            velocity.x = random_float( -100, +100 );
+            velocity.y = random_float( -100, +100 );
+            velocity.z = random_float( -100, +100 );
+        }
+        else
+        {
+            velocity.x = 0.0f;
+            velocity.y = 0.0f;
+            velocity.z = 0.0f;
+        }
+    }
+
+    PROTOCOL2_SERIALIZE_FUNCTION( stream )
+    {
+        serialize_float( stream, position.x );
+        serialize_float( stream, position.y );
+        serialize_float( stream, position.z );
+
+        bool at_rest = Stream::IsWriting && velocity.x == 0.0f && velocity.y == 0.0f && velocity.z == 0.0f;
+
+        serialize_bool( stream, at_rest );
+
+        if ( !at_rest )
+        {
+            serialize_float( stream, velocity.x );
+            serialize_float( stream, velocity.y );
+            serialize_float( stream, velocity.z );
+        }
+        else
+        {
+            if ( Stream::IsReading )
+            {
+                velocity.x = 0.0f;
+                velocity.y = 0.0f;
+                velocity.z = 0.0f;
+            }
+        }
+
+        return true;
+    }
+
+    bool operator == ( const TestPacketC & other )
+    {
+        return position.x == other.position.x &&
+               position.y == other.position.y &&
+               position.z == other.position.z &&
+               velocity.x == other.velocity.x &&
+               velocity.y == other.velocity.y &&
+               velocity.z == other.velocity.z;
+    }
+};
+
+struct TestPacketFactory : public protocol2::PacketFactory
+{
+    TestPacketFactory() : PacketFactory( TEST_PACKET_NUM_TYPES ) {}
+
+    protocol2::Packet* CreateInternal( int type )
+    {
+        switch ( type )
+        {
+            case TEST_PACKET_A: return new TestPacketA();
+            case TEST_PACKET_B: return new TestPacketB();
+            case TEST_PACKET_C: return new TestPacketC();
+        }
+        return NULL;
+    }
+};
+
+bool CheckPacketsAreIdentical( protocol2::Packet *p1, protocol2::Packet *p2 )
+{
+    assert( p1 );
+    assert( p2 );
+
+    if ( p1->GetType() != p2->GetType() )
+        return false;
+
+    switch ( p1->GetType() )
+    {
+        case TEST_PACKET_A:     return *((TestPacketA*)p1) == *((TestPacketA*)p2);
+        case TEST_PACKET_B:     return *((TestPacketB*)p1) == *((TestPacketB*)p2);
+        case TEST_PACKET_C:     return *((TestPacketC*)p1) == *((TestPacketC*)p2);
+        default:
+            return false;
+    }
+}
+
+int main()
+{
+    srand( time( NULL ) );
+
+    TestPacketFactory packetFactory;
+
+    // todo: same exact loop but instead of sending one packet at a time, use new functions "SendPackets", "ReceivePackets"
+
+    /*
+    for ( int i = 0; i < NumIterations; ++i )
+    {
+        const int packetType = rand() % TEST_PACKET_NUM_TYPES;
+
+        protocol2::Packet *writePacket = packetFactory.CreatePacket( packetType );
+
+        assert( writePacket );
+        assert( writePacket->GetType() == packetType );
+
+        uint8_t readBuffer[MaxPacketSize];
+        uint8_t writeBuffer[MaxPacketSize];
+
+        bool error = false;
+
+        const int bytesWritten = protocol2::WritePacket( writePacket, packetFactory, writeBuffer, MaxPacketSize, ProtocolId );
+
+        if ( bytesWritten > 0 )
+        {
+            printf( "wrote packet type %d (%d bytes)\n", writePacket->GetType(), bytesWritten );
+        }
+        else
+        {
+            printf( "write packet error\n" );
+            
+            error = true;
+        }
+
+        memset( readBuffer, 0, sizeof( readBuffer ) );
+        memcpy( readBuffer, writeBuffer, bytesWritten );
+
+        int readError;
+
+        protocol2::Packet *readPacket = protocol2::ReadPacket( packetFactory, readBuffer, bytesWritten, ProtocolId, NULL, &readError );
+        
+        if ( readPacket )
+        {
+            printf( "read packet type %d (%d bytes)\n", readPacket->GetType(), bytesWritten );
+
+            if ( !CheckPacketsAreIdentical( readPacket, writePacket ) )
+            {
+                printf( "read packet is not the same as written packet. something wrong with serialize function?\n" );
+
+                error = true;
+            }
+        }
+        else
+        {
+            printf( "read packet error: %s\n", protocol2::GetErrorString( readError ) );
+
+            error = true;
+        }
+
+        packetFactory.DestroyPacket( readPacket );
+        packetFactory.DestroyPacket( writePacket );
+
+        if ( error )
+            return 1;
+    }
+    */
+
+    return 0;
+}
--- premake4.lua
+++ premake4.lua
@@ -26,12 +26,18 @@ project "003_sending_large_blocks_of_data_quickly_and_reliably"
     kind "ConsoleApp"
     files { "003_sending_large_blocks_of_data_quickly_and_reliably.cpp", "protocol2.h", "network2.h" }
 
+project "004_packet_aggregation"
+    language "C++"
+    kind "ConsoleApp"
+    files { "004_packet_aggregation.cpp", "protocol2.h", "network2.h" }
+
 if _ACTION == "clean" then
     os.rmdir "obj"
     if not os.is "windows" then
         os.execute "rm -f 001_reading_and_writing_packets"
         os.execute "rm -f 002_packet_fragmentation_and_reassembly"
         os.execute "rm -f 003_sending_large_blocks_of_data_quickly_and_reliably"
+        os.execute "rm -f 004_packet_aggregation"
         os.execute "rm -f protocol2"
         os.execute "rm -f network2"
         os.execute "rm -f *.zip"
@@ -115,6 +121,21 @@ if not os.is "windows" then
         execute = function ()
             if os.execute "make -j4 003_sending_large_blocks_of_data_quickly_and_reliably" == 0 then
                 os.execute "./003_sending_large_blocks_of_data_quickly_and_reliably"
+            end
+        end
+    }
+
+    newaction
+    {
+        trigger     = "004",
+        description = "Build example source for packet aggregation",
+        valid_kinds = premake.action.get("gmake").valid_kinds,
+        valid_languages = premake.action.get("gmake").valid_languages,
+        valid_tools = premake.action.get("gmake").valid_tools,
+     
+        execute = function ()
+            if os.execute "make -j4 004_packet_aggregation" == 0 then
+                os.execute "./packet_aggregation"
             end
         end
     }
