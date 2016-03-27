e74f6c9b1ef0b0fbc1c663cfe3670782362cfc17
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-03-26 22:16:01 -0700

    gogogog

--- test.cpp
+++ test.cpp
@@ -1,7 +1,27 @@
 /*
-    Protocol 2 by Glenn Fiedler <glenn.fiedler@gmail.com>
-    This software is in the public domain. Where that dedication is not recognized, 
-    you are granted a perpetual, irrevocable license to copy, distribute, and modify this file as you see fit.
+    Functional Tests for Protocol2 Library and Network2 Library.
+
+    Copyright © 2016, The Network Protocol Company, Inc.
+    
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+           in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
+           from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #define NETWORK2_IMPLEMENTATION
@@ -113,6 +133,7 @@ struct TestData
     float float_value;
     double double_value;
     uint64_t uint64_value;
+    uint8_t bytes[17];
 };
 
 struct TestContext
@@ -147,6 +168,9 @@ struct TestObject : public protocol2::Object
         data.float_value = 3.1415926f;
         data.double_value = 1 / 3.0;   
         data.uint64_value = 0x1234567898765432L;
+
+        for ( int i = 0; i < sizeof( data.bytes ); ++i )
+            data.bytes[i] = rand() % 255;
     }
 
     PROTOCOL2_SERIALIZE_FUNCTION( stream )
@@ -177,6 +201,8 @@ struct TestObject : public protocol2::Object
         serialize_double( stream, data.double_value );
 
         serialize_uint64( stream, data.uint64_value );
+
+        serialize_bytes( stream, data.bytes, sizeof( data.bytes ) );
 
         serialize_check( stream, 0x12341111 );
 
